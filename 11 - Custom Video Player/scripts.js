67744a2761c056489fc5fb3eaa5e6dc075f86ff0
Jennifer Tchai <jentchai@gmail.com>
2022-04-21 17:36:08 -0400

    making play/pause functionality

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,27 @@
+// Get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build out functions
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    button.textContent
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+    console.log('update the button')
+}
+
+// Hook up the event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
